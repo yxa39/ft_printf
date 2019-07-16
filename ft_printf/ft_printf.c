0f0eac590406d21f272963fcbe122dad108625c1
Yujuan Xie <yxie@e1z3r3p26.42.us.org>
2019-07-16 11:59:34 -0700

    in progress

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,21 +6,37 @@
 /*   By: yxie <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/15 09:13:25 by yxie              #+#    #+#             */
-/*   Updated: 2019/07/15 12:35:50 by yxie             ###   ########.fr       */
+/*   Updated: 2019/07/16 11:58:52 by yxie             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <libc.h>
 
-
-char	*get_str(char *format, va_list *ap)
+void	print_csp(char *format, va_list *ap)
 {
-	int	a;
+	if (*format == 'c')
+		ft_putchar(va_arg(*ap, int));
+	if (*format == 's')
+		ft_putstr(va_arg(*ap, char *));
+	if (*format == 'p')
+		ft_printf("\n%#lx\n", (long)va_arg(*ap, void *));
+}
 
-	a = va_arg(*ap, int);
+void	print_diouxX(char *format, va_list *ap)
+{
+	if (*format == 'd' || *format == 'i')
+		ft_putstr(ft_itoa(va_arg(*ap, int)));
+	if (*format == 'o')
+		ft_printf(ft_itoa(ft_oct(va_arg(*ap, int))))
+}
 
-	format++;
-	return (ft_itoa(a));
+void	what_to_print(char *format, va_list *ap)
+{
+	if (ft_strchr("csp", *format))
+		print_csp(format, ap);
+	if (ft_strchr("diouxX", *format))
+		print_diouxX(format, ap);
 }
 
 int	ft_printf(const char * restrict format, ...)
@@ -34,11 +50,13 @@ int	ft_printf(const char * restrict format, ...)
 	{
 		if (*format == '%')
 		{
-			ft_putstr(get_str((char *)format, &ap));
+			format++;
+			what_to_print((char *)format, &ap);
 		}
 		else
 			ft_putchar(*format);
 		format++;
 	}
+	va_end(ap);
 	return (len);
 }
--- test/test.c
+++ test/test.c
@@ -6,12 +6,20 @@
 /*   By: yxie <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/15 10:21:00 by yxie              #+#    #+#             */
-/*   Updated: 2019/07/15 12:34:23 by yxie             ###   ########.fr       */
+/*   Updated: 2019/07/16 11:50:22 by yxie             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "test.h"
 int	main()
 {
-	ft_printf("abc%s", "ABC");
+	printf("ft_printf:\n");
+	ft_printf("aa%saa%s\n", "bb", "cc");
+	printf("aa%saa%s\n\n", "bb", "cc");
+	ft_printf("abc%c\n", 'c');
+	printf("abc%c\n\n", 'c');
+	ft_printf("abc%p\n", "abc");
+	printf("abc%p\n\n", "abc");
+	ft_printf("abc%d\n", 1);
+	printf("abc%d\n\n", 1);
 	return (0);
 }
